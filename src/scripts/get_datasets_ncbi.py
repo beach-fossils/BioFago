2fd0b0aea55a91ecd663940795f0ae05ca080c76
beach-fossils <jddmoura@gmail.com>
2024-03-06 15:10:41 +0000

    Update download_genome_dataset_with_retry function parameters
    The 'download_genome_dataset_with_retry' function has been updated to include additional parameters enabling it to also limit the datasets to annotated genomes and specify whether to limit it to RefSeq or GenBank genomes. Additionally, the 'unzip_genome_dataset' function has been commented out.

--- src/scripts/get_datasets_ncbi.py
+++ src/scripts/get_datasets_ncbi.py
@@ -15,13 +15,15 @@ def setup_logging(file_path):
         logging.getLogger().addHandler(logging.FileHandler(file_path))
 
 
-def download_genome_dataset_with_retry(keyword, output_directory, attempts=3, delay=5):
+def download_genome_dataset_with_retry(keyword, output_directory, attempts=3, delay=5, annotated=True, assembly_source='refseq'):
     """
     Attempts to download the dataset with retries.
     :param keyword: Search keyword for the dataset.
     :param output_directory: Directory to save the downloaded file.
     :param attempts: Number of retry attempts.
     :param delay: Delay between attempts in seconds.
+    :param annotated: Boolean indicating whether to limit to annotated genomes.
+    :param assembly_source: String indicating whether to limit to RefSeq or GenBank genomes ('refseq', 'genbank', or 'all').
     """
     command = [
         "datasets",
@@ -29,9 +31,20 @@ def download_genome_dataset_with_retry(keyword, output_directory, attempts=3, de
         "genome",
         "taxon",
         keyword,
+        # "--assembly-level",
+        # "complete",  # Filter for complete genome assemblies
+        "--assembly-source",
+        assembly_source,  # Filter for either 'RefSeq' (GCF_) or 'GenBank' (GCA_) genomes
+    ]
+
+    if annotated:
+        command.append("--annotated")
+
+    command.extend([
         "--filename",
         f"{output_directory}/ncbi_dataset.zip"
-    ]
+    ])
+
     for attempt in range(attempts):
         try:
             print(f"Attempt {attempt + 1} of {attempts}")
@@ -47,31 +60,32 @@ def download_genome_dataset_with_retry(keyword, output_directory, attempts=3, de
                 print("Maximum attempts reached. Download failed.")
 
 
-def unzip_genome_dataset(output_directory):
-    # Unzip the downloaded file
-    command = [
-        "unzip",
-        f"{output_directory}/ncbi_dataset.zip",
-        "-d",
-        output_directory
-    ]
-
-    try:
-        subprocess.run(command, check=True)
-        print("Genome dataset unzipped successfully!")
-    except subprocess.CalledProcessError as e:
-        print(f"Error unzipping dataset: {e}")
+# def unzip_genome_dataset(output_directory):
+#     # Unzip the downloaded file
+#     command = [
+#         "unzip",
+#         f"{output_directory}/ncbi_dataset.zip",
+#         "-d",
+#         output_directory
+#     ]
+#
+#     try:
+#         subprocess.run(command, check=True)
+#         print("Genome dataset unzipped successfully!")
+#     except subprocess.CalledProcessError as e:
+#         print(f"Error unzipping dataset: {e}")
 
 
 def main():
     # Example usage:
-    keyword = "Erwinia"
-    output_directory = "/Users/josediogomoura/Documents/BioFago/BioFago/data/all_genomes_erwinia/fasta"
-    log_file = "/Users/josediogomoura/Documents/BioFago/BioFago/data/all_genomes_erwinia/fasta/download.log"
+    keyword = "552"
+    output_directory = "/Users/josediogomoura/Documents/BioFago/BioFago/data/ApproachFlankGenes/genomes/ErwiniaAmyl"
+    log_file = "/Users/josediogomoura/Documents/BioFago/BioFago/data/ApproachFlankGenes/genomes/ErwiniaAmyl/download.log"
+    annotated = True
+    assembly_source = 'all'  # Use 'genbank' for GenBank or 'all' for both, 'refseq'
 
     setup_logging(log_file)
-    download_genome_dataset_with_retry(keyword, output_directory)
-    unzip_genome_dataset(output_directory)
+    download_genome_dataset_with_retry(keyword, output_directory, annotated=annotated, assembly_source=assembly_source)
 
 
 if __name__ == "__main__":
